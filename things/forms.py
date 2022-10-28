d6720bd7d351df8a38fd30320d177b400625fbc6
Richmond <richmond.bobie@outlook.com>
2022-10-28 14:57:30 +0100

    Create a form
    - Displayed a form on the home page

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,14 @@
 """Forms of the project."""
+from unittest.util import _MAX_LENGTH
+from django import forms
+
+from things.models import Thing
 
 # Create your forms here.
+
+class ThingForm(forms.ModelForm):
+    class Meta:
+        model = Thing
+        fields = ['name', 'description', 'quantity']
+        widgets = {'description': forms.Textarea}
+    
